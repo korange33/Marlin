b9843aa33c9baf77acff5963ffa196aeebe5f823
Richard Narbonne <richard.narbonne@gmail.com>
2015-06-10 10:08:55 +0200

    MANUAL_Z_HOME_POS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -376,7 +376,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // For deltabots this means top and center of the Cartesian print volume.
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 310 // For delta: Distance between nozzle and print surface after homing.
+#define MANUAL_Z_HOME_POS 310.8 // For delta: Distance between nozzle and print surface after homing.
+//#define MANUAL_Z_HOME_POS 311.5 // For delta: Distance between nozzle and print surface after homing.
+//#define MANUAL_Z_HOME_POS 305.8 // Just to test fake print
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
